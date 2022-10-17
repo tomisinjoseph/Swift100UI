0c167948e3946b14429770ce827211568a65d2c9
Tomisin Joseph <tomisin.joseph@clearscore.com>
2022-10-17 16:04:43 +0100

    Picker and ForEach

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -4,18 +4,24 @@
 //
 //  Created by Tomisin Joseph on 17/10/2022.
 //
-// views are a function of their state - that text field can only show something if it reflects a value stored in your program
 
-// two-way binding: we bind the text field so that it shows the value of our property, but we also bind it so that any changes to the text field also update the property.
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var tapCount = 0
 
+    let students = ["Harry", "Hermione", "Ron"]
+    
+    @State private var selectedStudent = "Harry"
+                    
     var body: some View {
-        Button("Tap Count: \(tapCount)") {
-            self.tapCount += 1
+        NavigationView {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) { // descriptive for screen readers
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
         }
     }
 }
