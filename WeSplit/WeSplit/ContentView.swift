2162ca3b6b42ba406fd9c205facb778f1779f149
Tomisin Joseph <tomisin.joseph@clearscore.com>
2022-10-17 16:33:32 +0100

    ForEach and TextField locale

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -4,24 +4,38 @@
 //
 //  Created by Tomisin Joseph on 17/10/2022.
 //
+// @State - automatically re-invoke the body property; it will reload your UI to reflect the changed state
+//  declarative user interface design - we say what we want rather than say how it should be done
 
 import SwiftUI
 
 struct ContentView: View {
-
-    let students = ["Harry", "Hermione", "Ron"]
-    
-    @State private var selectedStudent = "Harry"
                     
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
         NavigationView {
             Form {
-                Picker("Select your student", selection: $selectedStudent) { // descriptive for screen readers
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
                     }
                 }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }
             }
+            .navigationTitle("WeSplit")
         }
     }
 }
