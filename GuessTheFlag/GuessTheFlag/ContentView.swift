6f0c85b92bc1d596e4d85a45acdc9d88bf34069e
Tomisin Joseph <tomisin.joseph@clearscore.com>
2022-11-02 09:57:15 +0000

    Buttons

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -4,16 +4,38 @@
 //
 //  Created by Tomisin Joseph on 19/10/2022.
 //
-// angular gradients - start and end array with the same colour for a smooth colour transition
+// buttons and images - attach a role to adjust its appearance
+// built in styles exist too
 
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
-            .ignoresSafeArea()
+        VStack {
+            Button("button 1") { }
+                .buttonStyle(.bordered)
+            Button("button 2", role: .destructive) { }
+                .buttonStyle(.bordered)
+            Button("button 3") { }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            Button("button 4", role: .destructive) { }
+                .buttonStyle(.borderedProminent)
+            Button {
+                print("print button 5")
+            } label: {
+                Text("button 5")
+                    .padding()
+                    .foregroundColor(.pink)
+                    .background(.green)
+            }
+        }
     }
+}
+
+func executeDelete() {
+    print("Now deleting...")
 }
 
 struct ContentView_Previews: PreviewProvider {
